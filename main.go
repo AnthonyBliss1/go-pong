c5b5a38e7dcdf2006ab9a4eb9d5a83117e6fcce8
Anthony Bliss <anthony.bliss276@gmail.com>
2025-09-21 19:37:44 -0700

    added a brick and changed orientation

--- main.go
+++ main.go
@@ -18,12 +18,27 @@ const (
 	paddleSpeed  = 6
 )
 
+var (
+	Red   = color.RGBA{R: 255, G: 0, B: 0, A: 255}
+	Green = color.RGBA{R: 0, G: 255, B: 0, A: 255}
+	Blue  = color.RGBA{R: 0, G: 0, B: 255, A: 255}
+)
+
 type Object struct {
 	X, Y, W, H int
 }
 
 type Paddle struct {
 	Object
+}
+
+type Block struct {
+	Object
+	hits int
+}
+
+type Blocks struct {
+	Blocks []Block
 }
 
 type Ball struct {
@@ -35,6 +50,7 @@ type Ball struct {
 type Game struct {
 	paddle    Paddle
 	ball      Ball
+	blocks    Blocks
 	score     int
 	highScore int
 }
@@ -44,17 +60,31 @@ func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	paddle := Paddle{
 		Object: Object{
-			X: 600,
-			Y: 200,
-			W: 15,
-			H: 100,
+			X: screenWidth / 2,
+			Y: screenHeight - 30,
+			W: 100,
+			H: 15,
+		},
+	}
+
+	blocks := Blocks{
+		[]Block{
+			Block{
+				Object: Object{
+					X: (screenWidth / 2) - 25,
+					Y: 10,
+					W: 50,
+					H: 15,
+				},
+				hits: 0,
+			},
 		},
 	}
 
 	ball := Ball{
 		Object: Object{
-			X: 0,
-			Y: 0,
+			X: screenWidth / 2,
+			Y: screenHeight / 2,
 			W: 15,
 			H: 15,
 		},
@@ -65,6 +95,7 @@ func main() {
 	g := &Game{
 		paddle: paddle,
 		ball:   ball,
+		blocks: blocks,
 	}
 
 	err := ebiten.RunGame(g)
@@ -89,6 +120,33 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		color.White, false,
 	)
 
+	switch true {
+	case g.blocks.Blocks[0].hits == 0:
+		vector.DrawFilledRect(screen,
+			float32(g.blocks.Blocks[0].X), float32(g.blocks.Blocks[0].Y),
+			float32(g.blocks.Blocks[0].W), float32(g.blocks.Blocks[0].H),
+			color.White, false,
+		)
+	case g.blocks.Blocks[0].hits == 1:
+		vector.DrawFilledRect(screen,
+			float32(g.blocks.Blocks[0].X), float32(g.blocks.Blocks[0].Y),
+			float32(g.blocks.Blocks[0].W), float32(g.blocks.Blocks[0].H),
+			Green, false,
+		)
+	case g.blocks.Blocks[0].hits == 2:
+		vector.DrawFilledRect(screen,
+			float32(g.blocks.Blocks[0].X), float32(g.blocks.Blocks[0].Y),
+			float32(g.blocks.Blocks[0].W), float32(g.blocks.Blocks[0].H),
+			Blue, false,
+		)
+	case g.blocks.Blocks[0].hits == 3:
+		vector.DrawFilledRect(screen,
+			float32(g.blocks.Blocks[0].X), float32(g.blocks.Blocks[0].Y),
+			float32(g.blocks.Blocks[0].W), float32(g.blocks.Blocks[0].H),
+			Red, false,
+		)
+	}
+
 	scoreS := "Score: " + fmt.Sprint(g.score)
 	text.Draw(screen, scoreS, basicfont.Face7x13, 10, 10, color.White)
 
@@ -101,15 +159,16 @@ func (g *Game) Update() error {
 	g.ball.Move()
 	g.CollideWithWall()
 	g.CollideWithPaddle()
+	g.CollideWithBlock()
 	return nil
 }
 
 func (p *Paddle) MoveOnKeyPress() {
-	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) && p.Y <= screenHeight-p.H {
-		p.Y += paddleSpeed
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) && p.X <= screenWidth-p.W {
+		p.X += paddleSpeed
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) && p.Y >= 0 {
-		p.Y -= paddleSpeed
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) && p.X >= 0 {
+		p.X -= paddleSpeed
 	}
 }
 
@@ -126,23 +185,35 @@ func (g *Game) Reset() {
 }
 
 func (g *Game) CollideWithWall() {
-	if g.ball.X >= screenWidth {
+	if g.ball.Y+g.ball.H >= screenHeight {
 		g.Reset()
 	} else if g.ball.X <= 0 {
 		g.ball.dxdt = ballSpeed
 	} else if g.ball.Y <= 0 {
 		g.ball.dydt = ballSpeed
-	} else if g.ball.Y+g.ball.W >= screenHeight {
-		g.ball.dydt = -ballSpeed
+	} else if g.ball.X+g.ball.W >= screenWidth {
+		g.ball.dxdt = -ballSpeed
 	}
 }
 
 func (g *Game) CollideWithPaddle() {
-	if g.ball.X >= g.paddle.X-g.paddle.W && g.ball.Y >= g.paddle.Y && g.ball.Y <= g.paddle.Y+g.paddle.H {
-		g.ball.dxdt = -g.ball.dxdt
+	if g.ball.X+g.ball.W >= g.paddle.X && g.ball.X <= g.paddle.X+g.paddle.W && g.ball.Y+g.ball.H >= g.paddle.Y && g.ball.Y <= g.paddle.Y+g.paddle.H {
+		g.ball.dydt = -g.ball.dydt
+	}
+}
+
+func (g *Game) CollideWithBlock() {
+	if g.ball.X+g.ball.W >= g.blocks.Blocks[0].X && g.ball.X <= g.blocks.Blocks[0].X+g.blocks.Blocks[0].W && g.ball.Y+g.ball.H >= g.blocks.Blocks[0].Y && g.ball.Y <= g.blocks.Blocks[0].Y+g.blocks.Blocks[0].H {
+		g.ball.dydt = -g.ball.dydt
+		g.blocks.Blocks[0].hits++
 		g.score++
 		if g.score > g.highScore {
 			g.highScore = g.score
 		}
+		if g.blocks.Blocks[0].hits == 4 {
+			g.blocks.Blocks[0].X = 0 - g.blocks.Blocks[0].W
+			g.blocks.Blocks[0].Y = 0 - g.blocks.Blocks[0].H
+		}
+		fmt.Println("BLOCK HIT!")
 	}
 }
